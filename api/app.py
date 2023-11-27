d9185bb50d214a4d0c824382a3177571b1362b78
jsbailey <jason.bailey@imperial.ac.uk>
2023-11-27 17:53:19 +0000

    add my secret

--- api/app.py
+++ api/app.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
 @app.route("/")  # this sets the route to this page
 def home():
     db_user = os.environ["SECRET"]  # "cccc"
-    return render_template("index.html", text=db_user)
+    secret= os.environ["mysecret"]
+    return render_template("index.html", text=secret)
     # xxx return "This is the main page <h1>HELLO2</h1>"
 
 
